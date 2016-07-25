f8d808e6110a61c315c175e5f3edc59a9a6a27d9
Nina Holl <ninaholl2018@u.northwestern.edu>
2016-07-25 14:17:17 -0400

    Lots of updates

--- script.js
+++ script.js
@@ -8,8 +8,36 @@ $(document).ready(function(){
 	  url: url,
 	  method: 'GET',
 	}).done(function(result) {
-	  $("nyt").html(result.response.abstract);
+		console.log(result);
+		var myArray = result.results;
+		console.log("myArray:"+ myArray);
+		for (var i=0; i<myArray.length; i++) {
+			var title = myArray[i].title;
+			var abstract = myArray[i].abstract;
+			var url = myArray[i].url;
+			$("#article").append("<li><a href="+ url +">"+title+"</a></li>")
+			// console.log(myArray[i].abstract)
+			// console.log(myArray[i].url)
+			// $("#article").html(result.results[i].title.abstract);
+		  	// $("#abstract").html(result.results[i].abstract);
+}
 	}).fail(function(err) {
 	  throw err;
 	});
+	// function(){
+	// 		$.ajax({
+	// 			type: "GET",
+	// 			url: "https://newsapi.org/v1/articles",
+	// 			data: {
+	// 				source: thewashingtonpost
+	// 				apiKey: b1c7073d287e42f0920eaa19ba2f7150
+	// 			}
+	// 			success: function(response){
+	// 				console.log(response);
+					
+	// 			}
+	// 		});
+
+	// 	}
 });
+
